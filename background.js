edd2ed5a921a7d1181e80ad71d1012f07bf6b913
Tomii9273 <tomii6614@yahoo.co.jp>
2023-09-07 04:20:24 +0900

    format

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.tabs.query({}, (tabs) => {
       for (let t of tabs) {
         chrome.scripting.executeScript({
-          target: {tabId: t.id},
+          target: { tabId: t.id },
           func: insertWarningText,
         });
       }
@@ -13,6 +13,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 function insertWarningText() {
   const bodyContent = document.body.innerHTML;
-  const newContent = bodyContent.replace(/ソースコードは「Main.<i>拡張子<\/i>」で保存されます/g, 'ソースコードは「Main.<i>拡張子</i>」で保存されます\n<span style="color:red;">警告！！！！！！これはABCです！！！！！</span>');
+  const newContent = bodyContent.replace(
+    /ソースコードは「Main.<i>拡張子<\/i>」で保存されます/g,
+    'ソースコードは「Main.<i>拡張子</i>」で保存されます\n<span style="color:red;">警告！！！！！！これはABCです！！！！！</span>'
+  );
   document.body.innerHTML = newContent;
 }
